aed77d2299fa537e25d9bec7f28fe632115fdc29
Abhijeet-2003 <sinhaabhijeet200@gmail.com>
2023-01-30 22:54:30 +0530

    changes

---
+++ pages/ad.tsx
@@ -0,0 +1,16 @@
+import Nav from '@/comps/nav';
+import styles from '@/styles/Ad.module.css'
+
+const ad = () => {
+    return (
+        <div className={styles.ad}>
+            <Nav/>
+        </div>
+
+    );
+}
+ 
+export default ad;
+
+  
+//   ReactDOM.render(<MasterForm />, document.getElementById('root'))
---
+++ pages/success.tsx
@@ -0,0 +1,25 @@
+import Nav from "@/comps/nav";
+import styles from '@/styles/Success.module.css'
+import Check from "@/comps/check";
+
+const success = () => {
+    return (
+        <div className={styles.success}>
+            <Nav/>
+            <div className={styles.donut_cont}>
+                <div className={styles.donut}></div>
+                <div className={styles.success_cont}>
+                    <div className={styles.check}>
+                        <Check/>
+                        <div className={styles.success}>Success</div>
+                    </div>
+                    <div className={styles.text}>Consider what you’re advertising, and enter the most relevant page of your website. This might be your homepage, or  a more specific page.</div>
+                    <div className={styles.profile}><div>check profile</div></div>
+                </div>
+            </div>
+        </div>
+        
+    );
+}
+ 
+export default success;
