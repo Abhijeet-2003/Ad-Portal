f6e01c8bef3b206977175e61b0a8e883a522323e
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2023-02-02 21:45:54 +0530

    Add support for connecting to metamask

--- pages/landing.tsx
+++ pages/landing.tsx
@@ -1,8 +1,27 @@
 import Nav from '@/comps/nav';
 import styles from '@/styles/Landing.module.css'
 import meta from '@/assets//metamask.gif'
+import { connectMetamask } from '@/utils';
+import { useEffect, useState } from 'react';
 
 const Landing = () => {
+    const [windowDefined, setWindowDefined] = useState(false);
+    const [connected, setConnected] = useState(false);
+    // const 
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            setWindowDefined(true);
+            if (window.ethereum.selectedAddress) {
+                window.location.href = '/form3';
+                setConnected(true);
+            }
+        }
+    })
+    const handleConnectClick = async () => {
+        await connectMetamask();
+        setConnected(true);
+        setTimeout(() => {window.location.href = '/form3'}, 3000);
+    }
     return (
         <div className={styles.landing}>
             <Nav/>
@@ -14,7 +33,7 @@ const Landing = () => {
                         <div className={styles.desc}>Be seen by users at the very moment they do a transaction in a popular dapp using metamask wallet.</div>
                     </div>
                     <div className={styles.down}>
-                        <div className={styles.conn_button}><div>Connect Wallet</div></div>
+                        {windowDefined ? (connected ? <div className={styles.conn_button}><div>Connected</div></div> : <div className={styles.conn_button} onClick={handleConnectClick}><div>Connect Wallet</div></div>) : <div className={styles.conn_button}><div>Loading...</div></div>}
                         <div className={styles.text}>Get started with free personalised support. Create your custom ad plan with Metamask Ads</div>
                     </div>
                 </div>
