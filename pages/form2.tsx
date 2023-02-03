b38486ab003cda4c098ea6cf6fe4573173531f28
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2023-02-03 05:38:07 +0530

    Add file upload functionality

--- pages/form2.tsx
+++ pages/form2.tsx
@@ -4,10 +4,53 @@ import arrow from '@/assets/arrow.svg'
 import bullet from '@/assets/bullet.svg'
 import Link from 'next/link';
 import pic_drag from '@/assets/pic_drag.svg'
+import { useState, useCallback, FormEvent } from 'react';
+import axios from 'axios';
+import { useDropzone } from 'react-dropzone';
 
 
 
 const Form2 = () => {
+    const [fileImg, setFileImg] = useState<File | null>(null);
+    console.log(fileImg)
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({accept: {
+        'image/*': []
+      },
+      onDrop: acceptedFiles => {
+        setFileImg(acceptedFiles[0])
+      }
+    })
+    const sendFileToIPFS = async (e: FormEvent) => {
+        e.preventDefault();
+        if (fileImg) {
+            const formData = new FormData();
+
+            formData.append('file', fileImg);
+
+            const metadata = JSON.stringify({
+                name: 'Sample File Name',
+            });
+            formData.append('pinataMetadata', metadata);
+
+            const options = JSON.stringify({
+                cidVersion: 0,
+            })
+            formData.append('pinataOptions', options);
+            console.log("Running")
+            try {
+                const resFile = await axios.post('https://api.pinata.cloud/pinning/pinFileToIPFS', formData, {
+                    maxBodyLength: Infinity,
+                    headers: {
+                        'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+                        Authorization: `Bearer ${process.env.NEXT_PUBLIC_PINATA_JWT}`
+                    },
+                })
+                window.location.href = `/form3?cid=${resFile.data.IpfsHash}}`
+            } catch (error) {
+                console.log('Error: ', error)
+            }
+        }
+    }
 
     return (
         <div className={styles.form2}>
@@ -24,15 +67,17 @@ const Form2 = () => {
                 </div>
             </div>
 
-            <div className={styles.form_cont}>
+            <form className={styles.form_cont} onSubmit={sendFileToIPFS}>
 
-                <div className={styles.img_cont}>
-                    <img src={pic_drag.src} alt="" />
-                    <div className={styles.upl_cont}>
+                <div className={styles.img_cont} {...getRootProps()}>
+                    {fileImg && <img src={URL.createObjectURL(fileImg)} style={{width: "100%", height: "100%"}}/>}
+                    <input {...getInputProps()}/>
+                    {!fileImg ? <img src={pic_drag.src} alt="" /> : null}
+                    {!fileImg ?<div className={styles.upl_cont}>
                         <span className={styles.upl_text}>Upload a file</span>
                         <span className={styles.drag}> or drag and drop</span>
-                    </div>
-                    <div className={styles.limit_cont}>PNG, JPG, GIF up to 10MB</div>
+                    </div> : null}
+                    {!fileImg ? <div className={styles.limit_cont}>PNG, JPG, GIF up to 10MB</div> : null}
                 </div>
 
                 <div className={styles.text_cont}>
@@ -49,9 +94,9 @@ const Form2 = () => {
                     </div>
                 </div>
 
-                <div className={styles.proc_button}><Link href='/form3'>Proceed to Ad</Link></div>
+                <button className={styles.proc_button} type='submit'>Proceed to Ad</button>
 
-            </div>
+            </form>
 
         </div>
     );
