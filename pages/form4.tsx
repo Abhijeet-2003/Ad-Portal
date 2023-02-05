1a40932becef5e1438171efa09ae48971b64a8b4
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2023-02-05 15:36:13 +0530

    Add support for next api

--- pages/form4.tsx
+++ pages/form4.tsx
@@ -4,17 +4,37 @@ import styles from '@/styles/Form4.module.css'
 import Link from 'next/link';
 import { SyntheticEvent, useState } from 'react';
 import { useRouter } from 'next/router';
+import axios from 'axios';
 
 const Form4 = () => {
     const router = useRouter();
     console.log(router.query)
     const [website, setWebsite] = useState<string>();
-    const proceedToNext = (e: SyntheticEvent) => {
+    const proceedToNext = async (e: SyntheticEvent) => {
         e.preventDefault();
-        router.push({
-            pathname: '/form5',
-            query: { cid: router.query.cid, heading: router.query.heading, body: router.query.body, cta: router.query.cta, website: website }
-        })
+        if (typeof window !== 'undefined') {
+            const origin = window.location.origin;
+            console.log(origin)
+            const response = await axios.post(`${origin}/api/uploadAsTxt`, {
+                cid: router.query.cid,
+                heading: router.query.heading,
+                body: router.query.body,
+                cta: router.query.cta,
+                website: website
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            console.log('Response', response.data.hash)
+            if (response.data.hash) {
+                router.push({
+                    pathname: '/form5',
+                    query: { hash: response.data.hash }
+                })
+            }
+        }
     }
     return (
         <div className={styles.form4}>
