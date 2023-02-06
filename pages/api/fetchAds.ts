6587e9c9c4ee01beca2a1ecd3a122199efd057dd
AmreshSinha <35039730+AmreshSinha@users.noreply.github.com>
2023-02-06 23:16:35 +0530

    Fix API and add axios call for extra meta

--- pages/api/fetchAds.ts
+++ pages/api/fetchAds.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from '@prisma/client'
+import axios from "axios";
 
 const prisma = new PrismaClient()
 
@@ -14,7 +15,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 counts: true,
             }
         })
-        res.status(200).json({ads, error: false})
+        if (ads!.counts.length === 0) {
+            res.status(404).json({ message: 'No ads found', error: true })
+        }
+        let returnAds: unknown[] = []
+        for (const ad of ads!.counts) {
+        // ads!.counts.forEach(ad => {
+            const pinataRes = await axios.get(`https://gateway.pinata.cloud/ipfs/${ad.contentId}`)
+            const adName = pinataRes.data.name
+            const adImage = pinataRes.data.image
+            returnAds.push({ad, adName, adImage})
+        };
+        // console.log(returnAds)
+        res.status(200).json({ads: returnAds, error: false})
     } else {
         res.status(405).json({ message: 'Method not allowed', error: true })
     }
