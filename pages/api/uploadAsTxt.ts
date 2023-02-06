a6b9a70e4c25b740b4b3d44d1cf21c824e404792
AmreshSinha <35039730+AmreshSinha@users.noreply.github.com>
2023-02-07 00:34:53 +0530

    Fix upload with addition of Pinata SDK

--- pages/api/uploadAsTxt.ts
+++ pages/api/uploadAsTxt.ts
@@ -1,7 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import fs from 'fs';
 import path from 'path';
-import axios from 'axios';
+import pinataSDK from '@pinata/sdk';
+
+const pinata = new pinataSDK({pinataJWTKey: process.env.NEXT_PUBLIC_PINATA_JWT})
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
@@ -31,55 +33,58 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         }
 
         // Writing the new JSON to a txt file
-        fs.writeFile(path.join(process.cwd(), '/temp.txt'), JSON.stringify(newJsonBody), (err) => {
-            if (err) {
-                console.log(err)
-                res.status(500).json({ message: 'Internal server error', error: true })
-            }
-            console.log('JSON written to file successfully')
-        })
+        fs.writeFileSync(path.join(process.cwd(), '/temp.txt'), JSON.stringify(newJsonBody))
+        console.log('JSON written to file successfully')
 
         // Creating form data for uploading to Pinata
-        let formData = new FormData();
         const readStream = fs.createReadStream(path.join(process.cwd(), '/temp.txt'))
 
-        // Converting the readable stream to blob
-        const bufferArray: any[] = [];
-        readStream.on('data', (chunk: any) => {
-            bufferArray.push(chunk);
-        })
-        readStream.on('end', async () => {
-            const buffer = Buffer.concat(bufferArray);
-            const blob = new Blob([buffer], { type: 'text/plain' });
-            formData.append('file', blob)
-            const metadata = JSON.stringify({
+        console.log("Uploading file to IPFS...")
+        const options = {
+            pinataMetadata: {
                 name: `${newJsonBody.name}.txt`,
-            });
-            formData.append('pinataMetadata', metadata);
-            const options = JSON.stringify({
-                cidVersion: 0,
-            })
-            formData.append('pinataOptions', options);
-    
-            console.log("Uploading file to IPFS...")
-    
-            try {
-                const resFile = await axios.post('https://api.pinata.cloud/pinning/pinFileToIPFS', formData, {
-                    maxBodyLength: Infinity,
-                    headers: {
-                        'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
-                        Authorization: `Bearer ${process.env.NEXT_PUBLIC_PINATA_JWT}`
-                    },
-                })
-                if (resFile.status === 200) {
-                    console.log("File uploaded successfully to IPFS")
-                    res.status(200).json({ hash: resFile.data.IpfsHash, message: 'Success', error: false })
-                }
-            } catch (error) {
-                console.log('Error: ', error)
-                res.status(400).json({ message: 'Data not correct', error: true })
+            },
+            pinataOptions: {
+                cidVersion: 0 as const,
             }
-        })
+        }
+        await pinata.pinFileToIPFS(readStream, options)
+            .then((result) => {
+                console.log("File uploaded successfully to IPFS")
+                res.status(200).json({ hash: result.IpfsHash, message: 'Success', error: false })
+            })
+            .catch((err) => {
+                console.log('Error: ', err)
+                res.status(400).json({ message: 'Data not correct', error: true })
+            })
+            
+            // const metadata = JSON.stringify({
+            //     name: `${newJsonBody.name}.txt`,
+            // });
+            // formData.append('pinataMetadata', metadata);
+            // const options = JSON.stringify({
+            //     cidVersion: 0,
+            // })
+            // formData.append('pinataOptions', options);
+    
+    
+            // try {
+            //     const resFile = await axios.post('https://api.pinata.cloud/pinning/pinFileToIPFS', formData, {
+            //         maxBodyLength: Infinity,
+            //         headers: {
+            //             'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+            //             Authorization: `Bearer ${process.env.NEXT_PUBLIC_PINATA_JWT}`
+            //         },
+            //     })
+            //     if (resFile.status === 200) {
+            //         console.log("File uploaded successfully to IPFS")
+            //         res.status(200).json({ hash: resFile.data.IpfsHash, message: 'Success', error: false })
+            //     }
+            // } catch (error) {
+            //     console.log('Error: ', error)
+            //     res.status(400).json({ message: 'Data not correct', error: true })
+            // }
+        // })
 
     } else {
         // Handle any other HTTP method
