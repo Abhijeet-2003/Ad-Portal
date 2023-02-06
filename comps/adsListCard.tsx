ca7219fecb466fd85ccb3ab872a53296c14b36d9
AmreshSinha <35039730+AmreshSinha@users.noreply.github.com>
2023-02-07 00:35:23 +0530

    Fix props passing

--- comps/adsListCard.tsx
+++ comps/adsListCard.tsx
@@ -1,13 +1,13 @@
 import styles from '@/styles/adsListCard.module.scss'
 
-export default function AdsListCard() {
+export default function AdsListCard({ title, image}: {title: string, image: string}) {
     return (
         <>
             <div className={styles.card}>
                 <div className={styles.icon}>
-                    <img src="https://via.placeholder.com/150"/>
+                    <img src={image} />
                 </div>
-                <h3>Demo</h3>
+                <h3>{title}</h3>
             </div>
         </>
     )
